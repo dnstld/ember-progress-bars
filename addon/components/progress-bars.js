a8a8722ff04865a8303c74499f89739f3d4534e2
Denis Toledo <contato@denistoledo.com.br>
2020-03-03 15:25:28 -0300

    Back to glimmer components

--- addon/components/progress-bars.js
+++ addon/components/progress-bars.js
@@ -1,16 +1,13 @@
-import classic from 'ember-classic-decorator';
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from "@glimmer/tracking";
 
-@classic
-class ProgressBarsComponent extends Component {
+export default class ProgressBarsComponent extends Component {
   tagName = '';
 
-  value = this.value || 0;
+  @tracked value = this.args.value || 0;
 
   get translateX() {
     let translateValue = 100 - this.value;
     return `transform: translateX(-${translateValue}%)`;
   }
 }
-
-export default ProgressBarsComponent;
